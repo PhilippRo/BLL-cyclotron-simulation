eb833c853b97f28bf752996148a50654f81a7bc8
PhilippRo <philipp.rosendahl@gmx.de>
2016-11-04 17:39:05 +0100

    added output to buildscript

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 mkdir bin
+echo building Application
 g++ -c -I. -std=c++11 -fpermissive Application.cpp -o bin/Application.o
+echo building Cannel
 g++ -c -I. -std=c++11 -fpermissive Channel.cpp -o bin/Channel.o
+echo building double
 g++ -c -I. -std=c++11 -fpermissive Double.cpp -o bin/Double.o
+echo building Graph
 g++ -c -I. -std=c++11 -fpermissive Graph.cpp -o bin/Graph.o
+echo building Point
 g++ -c -I. -std=c++11 -fpermissive Point.cpp -o bin/Point.o
+echo building StdGraphController
 g++ -c -I. -std=c++11 -fpermissive StdGraphController.cpp -o bin/StdGraphController.o
+echo Widget
 g++ -c -I. -std=c++11 -fpermissive Widget.cpp -o bin/Widget.o
+echo Window
 g++ -c -I. -std=c++11 -fpermissive Window.cpp -o bin/Window.o
+echo Zyklotron
 g++ -c -I. -std=c++11 -fpermissive Zyklotron.cpp -o bin/Zyklotron.o
+echo ZyklotronController
 g++ -c -I. -std=c++11 -fpermissive ZyklotronController.cpp -o bin/ZyklotronController.o
+echo linking
 g++ -I. -std=c++11 -fpermissive main.cpp bin/*.o -o main -lboost_system -lboost_thread -lsfml-system -lsfml-window -lsfml-graphics
