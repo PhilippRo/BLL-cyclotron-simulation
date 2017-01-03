69608529c8b8a50ba363661e9ff5e6da8078fe0e
PhilippRo <philipp.rosendahl@gmx.de>
2017-01-03 14:23:59 +0100

    documented Zyklotron

--- include/Zyklotron.h
+++ include/Zyklotron.h
@@ -4,21 +4,6 @@
  *  Created on: 17.05.2016
  *      Author: philipp
  *
- *  Wird wie eine PointBuiĺder im Sachzusammenhang benutzt
- *
- *  Dadurch, dass private Felder in der Klassse Point nur durch Kinder der Klasse
- *  Point verändert werden können, aber in dem Programm nur die Klasse Zyklotron
- *  die Klasse Point beerbt, kann nur der Zyklotron Punkte verändern.
- *
- *  Das ist nötig, da der Zyklotron und das Window in verschieden Thread laufen werden
- *  und durch die Klaase Application verbunden werden, ein Thread sicheres Prinzip gefunden
- *  werden muss.
- *
- *  Ein Grundprinzip von Threadsicherheit is:
- *  	Einer Schreibt
- *  	Alle lesen.
- *
- *  Nur der Zyklotron kann Punkte schreiben, aber jeder kann Punkte lesen.
  */
 
 #ifndef ZYKLOTRON_H_
@@ -66,42 +51,150 @@ namespace ZyklotronParts{
 	};
 }
 
+
+/**
+	\brief das eigentliche Zyklotron
+
+	Diese Klasse stellt das eigentliche Zyklotron da und implementiert ein
+	relativisches und ein klassisches Modell für ein Zyklotron
+
+
+*/
 class Zyklotron {
 
-private :
+protected:
 
 	bool configured = false;
-	//static configureable values
-	Double q, u, d, v0, m0, b, f;
+	
+	///Ladung des Beschleunigten Teilchens
+	Double q;
 
-	//static non-personal configureable values
+	///Beschleunigungsspannung des Kondessators
+	Double u;
+
+	///Breite des Beschleunigungskondensators
+	Double d; 
+
+	///Eintritsgeschwindigkeit des Teilchens (Startgeschwindigkeit)
+	Double v0;
+
+	///Ruhemasse des Teilchens
+	Double m0;
+
+	///Stärke des Magnetfeldes
+	Double b;
+
+	///Frequenz, mit der das Vorzeichen der Spannung am Kondensator geändert wird
+	Double f;
+
+	///Beschleunigung im Kondensator
 	Double a0;
 
+	///Zeitmaßstab: gibt die Möglichkeit die Simulation zu verlangsamen oder zu beschleunigen
 	float timeScale = 1.0;
 
+	///Channel für die Kommunikation zwischen clalcThread und thisThread
 	BLL::Channel<ZyklotronParts::ZykSet>* chan;
+
+	///Auf dem clacThread läuft die eigentliche Berechnung
 	boost::thread *calcThread;
+
+	///Auf dem thisThread kommuniziert das Zyklotron mit dem Fenster
 	boost::thread *thisThread;
 
+	///Die Namen der Graphen, die von dem Zyklotron gefüllt werden
 	std::vector <std::string> names;
 
+	///Legt fest, ob das relativistische Modell verwendet wird
 	bool relativistic=true;
+
 public:
-	
+
+	///Standardkonstruktor	
 	Zyklotron();
+	
+	///Destruktor
+	/// fährt das Zyklotron runter
 	virtual ~Zyklotron();
 
+	/**
+		\brief konfiguriert das Zyklotron
+
+		Die Methode konfiguriert das Zyklotron mit den Werten
+
+		\param paraQ die Ladung des Teilchens
+
+		\param paraU die Behschleunigungsspannung
+
+		\param paraD die Breite des Beschleunigungskondensators
+
+		\param paraV0 die Eintrittsgeschwindigkeit in das Zyklotron
+
+		\param paraM0 die Ruhemasse des Teilchens
+
+		\param paraB Stärke des Magnetfeldes
+
+		\param paraF Frequenz mit der die Spannung am Kondensator geändert wird
+
+		\param PTimeScale der Zeitmaßstab
+
+		\param pRelativistic wählt das Modell (setzt relativistic)
+	*/
 	void configure(Double paraQ, Double paraU,
 			Double paraD, Double paraV0, Double paraM0, Double paraB, Double paraF, float PTimeScale, bool pRelativistic );
 
-void configure(Double paraQ, Double paraU,
+	/**
+		\brief konfiguriert das Zyklotron
+
+		Die Methode konfiguriert das Zyklotron mit den Werten
+
+		\param paraQ die Ladung des Teilchens
+
+		\param paraU die Behschleunigungsspannung
+
+		\param paraD die Breite des Beschleunigungskondensators
+
+		\param paraV0 die Eintrittsgeschwindigkeit in das Zyklotron
+
+		\param paraM0 die Ruhemasse des Teilchens
+
+		\param paraB Stärke des Magnetfeldes
+
+		\param paraF Frequenz mit der die Spannung am Kondensator geändert wird
+
+		\param PTimeScale der Zeitmaßstab
+	*/
+	void configure(Double paraQ, Double paraU,
 			Double paraD, Double paraV0, Double paraM0, Double paraB, Double paraF, float PTimeScale);
 
+	/**
+		\brief startet das Zyklotron
+
+		diese Methode startet das Zykotron und damit die Threads thisThread und
+		calcThread
+
+		Sie ließt aus dem Channel chan
+
+	*/
 	void run();
 
+
+	/**
+		\brief berechnet Werte
+
+		Diese Methode berechnet die Werte für die Simulation und ist der Code 
+		für den calcThread
+
+		Sie schreibt in den Channel chan
+	*/
 	void calc();
 
-	void setGraphNames(std::vector <std::string>);
+	/**
+		\brief Setter für die Graphnames
+
+		\param names Namen der Graphen
+	*/
+	void setGraphNames(std::vector <std::string> names );
 };
 
 } /* namespace BLL */
