4a34e9bdc264488ca6702417144a37510bccc3b1
PhilippRo <philipp.rosendahl@gmx.de>
2017-01-03 10:37:25 +0100

    documented Window class

--- include/Window.h
+++ include/Window.h
@@ -25,22 +25,53 @@ using namespace std;
 Das Hauptwidget für Window ist Graph
 */
 class Window{
+private:
+        /// Standardkonstructor (Singleton)
+	Window();
 
 protected:
+        /// eine Uhr, um die Framerate festzulegen, damit nicht zu viel Zeit auf
+	/// das Rendern der Graphen verschwendet wird
 	sf::Clock renderTimer;
+
+        /// ein Event, das lokal verwendet wird, um das Fenster auf Tastendruck zu
+        /// schleißen
 	sf::Event ev;
+
+	/// die Liste von Graphen, die verwaltet und gerendert werden
 	vector<Graph> graph;
+
+	/// eine Liste von Graphname, damit die Graphen verwaltet werden können
+        /// der n-te Entrag der Liste ist der Name des n-ten Graphen
 	vector<string> graphName;
+
+        /// der lokale GraphController, der die Anzahl der Punkte komprimiert.
 	vector<StdGraphController*> graphController;
+
+	/// Variabel für die Geometrie
 	unsigned int graphsInARow = 4;
 
 public:
+
+	/// Fenster, um die Werte zu rendern
 	sf::RenderWindow* win;
 
-	Window();
+
+  
+	/// Destruktor
 	virtual ~Window();
 
 	// templates kompilieren nicht in einem cpp file
+	/**
+		\brief Fügt einen Graphen hinzu
+
+		Fügt einen Graphen mit dem GraphController t zu dem Window hinzu
+
+		\param name Name des Graphen
+
+		\tparam t Typ des GraphController des Graphen im Fenster
+
+	*/
 	template<class t>
 	void addGraph(std::string name){
 		this->graphName.push_back(name);
@@ -49,11 +80,42 @@ public:
 		this->graph.push_back(g);
 	}
 
+	/**
+		\brief Giebt einen Graphen zurück
+
+		Gibt einen Graphen zurück, um dannach Punkte hinzuzufügen.
+
+		\param name Name des Graphen
+
+		\return der entsprechende Graph
+	*/
 	Graph* getGraph(std::string name);
+
+	/**
+		\brief rendert die Punkte
+
+		ruft für jeden Graphen die Methode render auf
+	*/
 	void render();
+
+	/**
+		\brief erstellt das Fenster
+
+		ruft den Algorithmus für die Geometrie auf und überträgt die 
+		errechneten Dimentsionen auf die Graphen
+	*/
 	void create();
+
+	/**
+		\brief Mainloop des Fensters
+
+		ruft die Methode Window::render auf, die nur 25 mal in der Sekunde 
+		rendert und wartet auf einen Tastendruck, um dannach das Fenster zu 
+		schließen.
+	*/
 	void mainloop();
 
+	///Singleton typische Methode
 	static Window& instance(){
 		static Window win;
 		return win;
@@ -61,5 +123,6 @@ public:
 };
 
 } /* namespace BLL */
+
 
 #endif /* WINDOW_H_ */
